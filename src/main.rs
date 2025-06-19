4873f6a6714079c94b7fcd7cfc7e3ed19874eb44
Kevin <kzhang366@gmail.com>
2025-06-19 11:34:36 -0600

    tcplistener

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use std::net::{TcpListener, TcpStream};
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
 
+    for stream in listener.incoming() {
+        let stream = stream?;
+
+        println!("Connected");
+    }
+
     Ok(())
 }
 
