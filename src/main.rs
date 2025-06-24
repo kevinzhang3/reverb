a64b24e0c97a98d4e6395885cf6f19cbe2d84c5b
Kevin <kzhang366@gmail.com>
2025-06-24 11:51:43 -0600

    adding server as an io event source and registering to the Poll instance

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,18 @@ use mio::net::{TcpListener, TcpStream};
 use mio::{Events, Interest, Poll, Token};
 
 
+const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
+
 fn main() -> Result<(), Box<dyn Error>> {
-    
+    let mut poll = Poll::new()?;
+
+    let mut events = Events::with_capacity(128);
+
+    let addr = "127.0.0.1:8080".parse()?;
+    let mut server = TcpListener::bind(addr)?;
+
+    poll.registry().register(&mut server, SERVER, Interest::READABLE)?;
 
     Ok(())
 }
