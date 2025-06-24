4f652637dc7eb320dd449683a6f17a461b310912
Kevin <kzhang366@gmail.com>
2025-06-24 17:02:18 -0600

    basic server skeleton

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,36 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let addr = "127.0.0.1:8080".parse()?;
     let mut server = TcpListener::bind(addr)?;
-
     poll.registry().register(&mut server, SERVER, Interest::READABLE)?;
 
-    Ok(())
+    let mut client = TcpStream::connect(addr)?;
+    poll.registry().register(&mut client, CLIENT, Interest::READABLE | Interest::WRITABLE)?;
+
+    loop {
+        poll.poll(&mut events, None)?;
+
+        // event processing
+        for event in events.iter() {
+            
+            // match it based on the provided token 
+            match event.token() {
+                SERVER => {
+                    let connection = server.accept();
+                    drop(connection);
+                }
+                CLIENT => {
+                    if event.is_writable() {
+                        // write stuff
+                    }
+
+                    if event.is_readable() {
+                        // read stuff 
+                    }
+
+                    return Ok(());
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
 }
