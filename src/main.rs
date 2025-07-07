292d63959ea7841c7b34d80496811beb70999ab1
Kevin <kzhang366@gmail.com>
2025-07-07 14:23:52 -0600

    graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
@@ -7,48 +6,54 @@ use hyper::http::{Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 use anyhow::{Result, Context};
+use tokio::fs;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Server running on http://127.0.0.1:8080");
 
-    loop {
-        let (stream, _) = listener.accept().await?;
+    tokio::select! {
+        _ = async {
+            loop {
+                let (stream, _) = listener.accept().await?;
+                let io = TokioIo::new(stream);
 
-        let io = TokioIo::new(stream);
-
-        tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .keep_alive(true)
-                .serve_connection(io, service_fn(router))
-                .await
-            {
-                eprint!("Error serving connection: {:?}", err);
+                tokio::task::spawn(async move {
+                    if let Err(err) = http1::Builder::new()
+                        .keep_alive(true)
+                        .serve_connection(io, service_fn(router))
+                        .await
+                    {
+                        eprintln!("Error serving connection: {:?}", err);
+                    }
+                });
             }
-        });
-    }
-}
 
-async fn router(request: Request<hyper::body::Incoming>) -> hyper::Result<Response<Full<Bytes>>> {
-    eprint!("Request: {:#?}", request);
+            #[allow(unreachable_code)]
+            Ok::<_, anyhow::Error>(())
+        } => {},
+
+        _ = tokio::signal::ctrl_c() => {
+            println!("Shutting down.");
+        }
+    }
+
+    Ok(())
+}
+async fn router(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>> {
+    eprintln!("Request: {:#?}", request);
 
     let response = match request.uri().path() {
-        "/" => Response::builder()
-            .status(200)
-            .header("foo", "bar")
-            .body(Full::new(Bytes::from(fs::read_to_string("rust.html").unwrap())))
-            .unwrap(),
-        _ => Response::builder()
-            .status(404)
-            .body(Full::new(Bytes::from(fs::read_to_string("404.html").unwrap())))
-            .unwrap()
+        "/" => base_uri().await?,
+        _ => not_found().await?
     };
 
     Ok(response)
 }
 
-fn base_uri() -> Result<Response<Full<Bytes>>> {
-    let contents = fs::read_to_string("rust.html")
+async fn base_uri() -> Result<Response<Full<Bytes>>> {
+    let contents = fs::read_to_string("rust.html").await
         .context("Failed to read HTML")?;
     let body = Full::new(Bytes::from(contents));
 
@@ -60,6 +65,15 @@ fn base_uri() -> Result<Response<Full<Bytes>>> {
     Ok(response)
 }
 
-fn not_found() {
-    let contents = fs::read_to_string("404.html")
+async fn not_found() -> Result<Response<Full<Bytes>>> {
+    let contents = fs::read_to_string("404.html").await
+        .context("Failed to read HTML")?;
+    let body = Full::new(Bytes::from(contents));
+
+    let response = Response::builder()
+        .status(404)
+        .body(body)
+        .context("Failed to build response")?;
+
+    Ok(response)
 }
