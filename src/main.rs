b967fa7d058af574ba8e3bb2ee77a27bac3af221
Kevin <kzhang366@gmail.com>
2025-07-08 11:51:40 -0600

    copying echo api

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,13 @@
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::http::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
-use anyhow::{Result, Context};
-use tokio::fs;
+use anyhow::Result;
+
 mod router;
 
 
-// default runtime uses the N threads where N = num of cores
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut router = router::Router::new();
-
-    server("127.0.0.1:8080").await?;
-
-    Ok(())
-}
-
-
-async fn server(port: &str) -> Result<()> {
-    let listener = TcpListener::bind(port).await?;
-    println!("Server running on http://{}", port);
-
-    tokio::select! {
-        _ = async {
-            loop {
-                let (stream, _) = listener.accept().await?;
-                let io = TokioIo::new(stream);
-
-                tokio::task::spawn(async move {
-                    if let Err(err) = http1::Builder::new()
-                        .keep_alive(true)
-                        .serve_connection(io, service_fn(serve))
-                        .await
-                    {
-                        eprintln!("Error serving connection: {:?}", err);
-                    }
-                });
-            }
-
-            #[allow(unreachable_code)]
-            Ok::<_, anyhow::Error>(())
-        } => {},
-
-        _ = tokio::signal::ctrl_c() => {
-            println!("\nShutting down.");
-        }
-    }
-
-    Ok(())
-}
-
-async fn serve(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>> {
-    eprintln!("{:#?}", request);
-
-    let response = match request.uri().path() {
-        "/" => base_uri().await?,
-        _ => not_found().await?
-    };
-
-    Ok(response)
-}
-
-async fn base_uri() -> Result<Response<Full<Bytes>>> {
-    let contents = fs::read_to_string("rust.html").await
-        .context("Failed to read HTML")?;
-    let body = Full::new(Bytes::from(contents));
-
-    let response = Response::builder()
-        .status(200)
-        .body(body)
-        .context("Failed to build response")?;
     
-    Ok(response)
-}
+    router.add()
 
-async fn not_found() -> Result<Response<Full<Bytes>>> {
-    let contents = fs::read_to_string("404.html").await
-        .context("Failed to read HTML")?;
-    let body = Full::new(Bytes::from(contents));
-
-    let response = Response::builder()
-        .status(404)
-        .body(body)
-        .context("Failed to build response")?;
-
-    Ok(response)
+    Ok(())
 }
--- src/router.rs
+++ src/router.rs
@@ -4,6 +4,11 @@ use hyper::body::Bytes;
 use hyper::http::{Request, Response};
 use anyhow::Result;
 use std::collections::HashMap;
+use tokio::net::TcpListener;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use std::sync::Arc;
+use hyper_util::rt::TokioIo;
 
 pub type Handler = fn(request: Request<hyper::body::Incoming>) -> BoxFuture<'static, Result<Response<Full<Bytes>>>>;
 
@@ -20,5 +25,56 @@ impl Router {
 
     pub fn add(&mut self, path: &str, handler: Handler) {
         self.routes.insert(path.to_string(), handler);
+    }
+
+    pub fn handle(&self, req: Request<hyper::body::Incoming>) -> BoxFuture<'static, Result<Response<Full<Bytes>>>> {
+        eprintln!("Request: {:?}", req);
+
+        if let Some(handler) = self.routes.get(req.uri().path()) {
+            handler(req)
+        } else {
+            Box::pin(async {
+                Ok(Response::builder()
+                    .status(404)
+                    .body(Full::new(Bytes::from("Not Found")))
+                    .unwrap())
+            })
+        } 
+    }
+
+    pub async fn start(self, port: &str) -> Result<()> {
+        let router = std::sync::Arc::new(self);
+
+        let listener = TcpListener::bind(port).await?;
+        println!("Server running on http://{}", port);
+
+        tokio::select! {
+            _ = async {
+                loop {
+                    let (stream, _) = listener.accept().await?;
+                    let io = TokioIo::new(stream);
+                    let clone = Arc::clone(&router);
+
+                    tokio::task::spawn(async move {
+                        if let Err(err) = http1::Builder::new()
+                            .keep_alive(true)
+                                .serve_connection(io, service_fn(move |req| clone.handle(req)))
+                                .await
+                        {
+                            eprintln!("Error serving connection: {:?}", err);
+                        }
+                    });
+                }
+
+                #[allow(unreachable_code)]
+                Ok::<_, anyhow::Error>(())
+            } => {},
+
+            _ = tokio::signal::ctrl_c() => {
+                println!("\nShutting down.");
+            }
+        }
+
+        Ok(())
     }
 }
