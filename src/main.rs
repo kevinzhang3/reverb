a1979973880e5600cf05e4b48dc673bc3e4ca9e4
kevinzhang3 <kzhang366@gmail.com>
2025-07-28 13:59:27 -0600

    demo

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-use anyhow::Result;
-use hyper::body::Incoming;
 use reverb::{
     routing::Router,
     response::Response,
     util::{DataFormat, HttpStatus}
 };
 
-fn greet(_req: hyper::Request<Incoming>) -> Response {
+fn greet(_req: hyper::Request<hyper::body::Incoming>) -> Response {
     return Response::new(DataFormat::JSON("Hello, world!".to_string()), HttpStatus::Ok); 
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     let mut router = Router::new();
 
     router.serve_static("/", "public");
     router.get("/greet", greet);
     router.debug(true);
-    router.start("127.0.0.1:8080").await?;
-
-    Ok(())
+    router.start("127.0.0.1:8080").await.unwrap();
 }
