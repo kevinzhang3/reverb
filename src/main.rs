e3a707470e4fc01722d9a30c99031e229e7ca95d
kevinzhang3 <kzhang366@gmail.com>
2025-07-28 13:46:10 -0600

    ok

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<()> {
     let mut router = Router::new();
 
     router.serve_static("/", "public");
-    router.get("/api", greet);
+    router.get("/greet", greet);
     router.debug(true);
     router.start("127.0.0.1:8080").await?;
 
